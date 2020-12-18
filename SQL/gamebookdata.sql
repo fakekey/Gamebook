d053f9b3b08e93f383cf19b74d631341a401a6ed
Duc Long DTH <longvkook0312@gmail.com>
2020-12-18 10:47:28 +0700

    thay doi sql

--- SQL/gamebookdata.sql
+++ SQL/gamebookdata.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100414
  File Encoding         : 65001
 
- Date: 13/12/2020 22:04:25
+ Date: 18/12/2020 10:47:08
 */
 
 SET NAMES utf8mb4;
@@ -35,77 +35,71 @@ CREATE TABLE `account`  (
 -- Records of account
 -- ----------------------------
 INSERT INTO `account` VALUES ('admin', '21232f297a57a5a743894a0e4a801fc3', '1');
-INSERT INTO `account` VALUES ('camdalavip@gmail.com', '1', '2');
-INSERT INTO `account` VALUES ('caonam123@gmail.com', '2', '2');
-INSERT INTO `account` VALUES ('chauvv@gmail.com', '21232f297a57a5a743894a0e4a801fc3', '3');
-INSERT INTO `account` VALUES ('khanhnt@gmail.com', '21232f297a57a5a743894a0e4a801fc3', '3');
-INSERT INTO `account` VALUES ('noobkhach2', '2', '3');
+INSERT INTO `account` VALUES ('camdalavip', '10a96d94b77bcaecb95fdabb4c2b9fa6', '2');
+INSERT INTO `account` VALUES ('CaoNam123@gmail.com', '24dca22fdab7a594baa005d55db4f7bf', '2');
+INSERT INTO `account` VALUES ('longVippro', '3354045a397621cd92406f1f98cde292', '1');
+INSERT INTO `account` VALUES ('NamABC@job.com', '24dca22fdab7a594baa005d55db4f7bf', '2');
+INSERT INTO `account` VALUES ('NamCH@job.com', '24dca22fdab7a594baa005d55db4f7bf', '2');
+INSERT INTO `account` VALUES ('NamNamHiHi@job.com', '24dca22fdab7a594baa005d55db4f7bf', '2');
+INSERT INTO `account` VALUES ('Null@gmail.com', '24dca22fdab7a594baa005d55db4f7bf', '2');
+INSERT INTO `account` VALUES ('tranct1@gmail.com', '24dca22fdab7a594baa005d55db4f7bf', '3');
+INSERT INTO `account` VALUES ('tranct2@gmail.com', '24dca22fdab7a594baa005d55db4f7bf', '3');
+INSERT INTO `account` VALUES ('tranct3@gmail.com', '24dca22fdab7a594baa005d55db4f7bf', '3');
+INSERT INTO `account` VALUES ('tranct@gmail.com', '24dca22fdab7a594baa005d55db4f7bf', '3');
 
 -- ----------------------------
 -- Table structure for chi tiet hoa don
 -- ----------------------------
 DROP TABLE IF EXISTS `chi tiet hoa don`;
 CREATE TABLE `chi tiet hoa don`  (
-  `ID_cthd` int(20) NOT NULL AUTO_INCREMENT,
-  `MAHD` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `ID_sp` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `ID_cthd` int(200) NOT NULL AUTO_INCREMENT,
+  `MAHD` int(100) NOT NULL,
+  `ID_sp` int(100) NOT NULL,
   `Don gia` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `ID_tinhtrang` int(10) NOT NULL DEFAULT 1,
   PRIMARY KEY (`ID_cthd`) USING BTREE,
   INDEX `cthd_hd`(`MAHD`) USING BTREE,
   INDEX `cthd_sp`(`ID_sp`) USING BTREE,
   INDEX `cthd_ttsp`(`ID_tinhtrang`) USING BTREE,
+  CONSTRAINT `cthd_ttsp` FOREIGN KEY (`ID_tinhtrang`) REFERENCES `tinh trang don hang` (`ID_tinhtrang`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `cthd_hd` FOREIGN KEY (`MAHD`) REFERENCES `hoa don` (`MAHD`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `cthd_sp` FOREIGN KEY (`ID_sp`) REFERENCES `san pham` (`ID_sp`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `cthd_ttsp` FOREIGN KEY (`ID_tinhtrang`) REFERENCES `tinh trang don hang` (`ID_tinhtrang`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  CONSTRAINT `sthd` FOREIGN KEY (`ID_sp`) REFERENCES `san pham` (`ID_sp`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of chi tiet hoa don
--- ----------------------------
-INSERT INTO `chi tiet hoa don` VALUES (3, 'HD02', 'SP02', '0', 1);
-INSERT INTO `chi tiet hoa don` VALUES (4, 'HD02', 'SP03', '0', 1);
 
 -- ----------------------------
 -- Table structure for hoa don
 -- ----------------------------
 DROP TABLE IF EXISTS `hoa don`;
 CREATE TABLE `hoa don`  (
-  `MAHD` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `MAKH` int(10) NOT NULL,
+  `MAHD` int(100) NOT NULL AUTO_INCREMENT,
+  `MAKH` int(100) NOT NULL,
   `Ngay Mua` date NOT NULL,
   `Tong Tien` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`MAHD`) USING BTREE,
   INDEX `hd_kh`(`MAKH`) USING BTREE,
   CONSTRAINT `hd_kh` FOREIGN KEY (`MAKH`) REFERENCES `khach hang` (`MAKH`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of hoa don
--- ----------------------------
-INSERT INTO `hoa don` VALUES ('HD02', 2, '2020-12-14', '');
-INSERT INTO `hoa don` VALUES ('HD04', 6, '2020-12-21', '');
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for khach hang
 -- ----------------------------
 DROP TABLE IF EXISTS `khach hang`;
 CREATE TABLE `khach hang`  (
-  `MAKH` int(10) NOT NULL AUTO_INCREMENT,
+  `MAKH` int(100) NOT NULL AUTO_INCREMENT,
   `Email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `SĐT` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `Họ Tên` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`MAKH`) USING BTREE,
   INDEX `kh_acc`(`Email`) USING BTREE,
   CONSTRAINT `kh_acc` FOREIGN KEY (`Email`) REFERENCES `account` (`Email`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of khach hang
 -- ----------------------------
-INSERT INTO `khach hang` VALUES (2, 'chauvv@gmail.com', '18888888', 'Vũ Vương Châu');
-INSERT INTO `khach hang` VALUES (3, 'khanhnt@gmail.com', '12332444', 'Nguyễn Thu Khanh');
-INSERT INTO `khach hang` VALUES (6, 'noobkhach2', '', 'Nooby2');
+INSERT INTO `khach hang` VALUES (16, 'tranct@gmail.com', 'Hihi');
+INSERT INTO `khach hang` VALUES (17, 'tranct1@gmail.com', 'Nam Cao ');
+INSERT INTO `khach hang` VALUES (18, 'tranct2@gmail.com', 'Nam Cao ');
+INSERT INTO `khach hang` VALUES (19, 'tranct3@gmail.com', 'Nam1');
 
 -- ----------------------------
 -- Table structure for nhan vien
@@ -118,14 +112,19 @@ CREATE TABLE `nhan vien`  (
   PRIMARY KEY (`MANV`) USING BTREE,
   INDEX `nv_acc`(`Email`) USING BTREE,
   CONSTRAINT `nv_acc` FOREIGN KEY (`Email`) REFERENCES `account` (`Email`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 18 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of nhan vien
 -- ----------------------------
 INSERT INTO `nhan vien` VALUES (1, 'Admin', 'admin');
-INSERT INTO `nhan vien` VALUES (4, 'Cam Da', 'camdalavip@gmail.com');
-INSERT INTO `nhan vien` VALUES (5, 'Nam', 'caonam123@gmail.com');
+INSERT INTO `nhan vien` VALUES (10, 'Đức Long', 'longVippro');
+INSERT INTO `nhan vien` VALUES (12, 'Lã Quốc Nghị', 'camdalavip');
+INSERT INTO `nhan vien` VALUES (13, 'Cao Hoài Nam', 'CaoNam123@gmail.com');
+INSERT INTO `nhan vien` VALUES (14, 'NamCH', 'NamCH@job.com');
+INSERT INTO `nhan vien` VALUES (15, 'Nam 1', 'NamNamHiHi@job.com');
+INSERT INTO `nhan vien` VALUES (16, 'Cu Nam', 'NamABC@job.com');
+INSERT INTO `nhan vien` VALUES (17, 'Cao Cao', 'Null@gmail.com');
 
 -- ----------------------------
 -- Table structure for quyen
@@ -149,30 +148,30 @@ INSERT INTO `quyen` VALUES ('3', 'Khách Hàng');
 -- ----------------------------
 DROP TABLE IF EXISTS `san pham`;
 CREATE TABLE `san pham`  (
-  `ID_sp` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `ID_sp` int(100) NOT NULL AUTO_INCREMENT,
   `title` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `category` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `version` varchar(35) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `img_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `price` float(10, 2) NOT NULL,
   `date release` date NOT NULL,
-  `img_path` varchar(125) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `img_path` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`ID_sp`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of san pham
 -- ----------------------------
-INSERT INTO `san pham` VALUES ('SP01', 'Half Life 3', 'AAA', '3.0', 'SP01', 67.00, '2020-01-25', NULL);
-INSERT INTO `san pham` VALUES ('SP02', 'Grand Theft Auto VI', 'AAA', '4.125', 'SP02', 30.00, '2020-10-15', NULL);
-INSERT INTO `san pham` VALUES ('SP03', 'Star Citizen', 'AAA', '2.16', 'SP03', 1.91, '2020-07-23', NULL);
-INSERT INTO `san pham` VALUES ('SP04', 'Star Wars Jedi: Fallen Order 2', 'AAA', '2.0.1', 'SP04', 0.00, '2020-10-25', NULL);
-INSERT INTO `san pham` VALUES ('SP05', 'Project Athia', 'AAA', '2.638', 'SP05', 0.00, '2020-08-03', NULL);
-INSERT INTO `san pham` VALUES ('SP06', 'The Elder Scrolls 6', 'AAA', '6.0.1', 'SP06', 44.97, '2020-05-13', NULL);
-INSERT INTO `san pham` VALUES ('SP07', 'Homeworld 3', 'AAA', '3.1.12', 'SP07', 6.99, '2020-09-01', NULL);
-INSERT INTO `san pham` VALUES ('SP08', 'Fable 2020', 'AAA', '1.3.25', 'SP08', 0.00, '2020-06-01', NULL);
-INSERT INTO `san pham` VALUES ('SP09', 'Dragon Age 4 The Dread Wolf Rise', 'AAA', '4.1.1', 'SP09', 10.00, '2020-07-20', NULL);
-INSERT INTO `san pham` VALUES ('SP10', 'Suicide Squad: Kill the Justice League', 'AAA', '2.3.5', 'SP10', 0.00, '2020-03-27', NULL);
+INSERT INTO `san pham` VALUES (1, 'Half Life 3', 'AAA', '3.0', 67.00, '2020-01-25', 'D:\\báo cáo EPU\\Đồ Án .Net\\Code\\Gamebook\\Server\\Resources\\img\\forgotPass.png');
+INSERT INTO `san pham` VALUES (2, 'Grand Theft Auto VI', 'AAA', '4.125', 30.00, '2020-10-15', 'D:\\báo cáo EPU\\Đồ Án .Net\\Code\\Gamebook\\Server\\Resources\\img\\forgotPass.png');
+INSERT INTO `san pham` VALUES (3, 'Star Citizen', 'AAA', '2.16', 1.91, '2020-07-23', 'D:\\báo cáo EPU\\Đồ Án .Net\\Code\\Gamebook\\Server\\Resources\\img\\forgotPass.png');
+INSERT INTO `san pham` VALUES (4, 'Star Wars Jedi: Fallen Order 2', 'AAA', '2.0.1', 0.00, '2020-10-25', 'D:\\báo cáo EPU\\Đồ Án .Net\\Code\\Gamebook\\Server\\Resources\\img\\forgotPass.png');
+INSERT INTO `san pham` VALUES (5, 'Project Athia', 'AAA', '2.638', 0.00, '2020-08-03', 'D:\\báo cáo EPU\\Đồ Án .Net\\Code\\Gamebook\\Server\\Resources\\img\\forgotPass.png');
+INSERT INTO `san pham` VALUES (6, 'The Elder Scrolls 6', 'AAA', '6.0.1', 44.97, '2020-05-13', 'D:\\báo cáo EPU\\Đồ Án .Net\\Code\\Gamebook\\Server\\Resources\\img\\forgotPass.png');
+INSERT INTO `san pham` VALUES (7, 'Homeworld 3', 'AAA', '3.1.12', 6.99, '2020-09-01', 'D:\\báo cáo EPU\\Đồ Án .Net\\Code\\Gamebook\\Server\\Resources\\img\\forgotPass.png');
+INSERT INTO `san pham` VALUES (8, 'Fable 2020', 'AAA', '1.3.25', 0.00, '2020-06-01', 'D:\\báo cáo EPU\\Đồ Án .Net\\Code\\Gamebook\\Server\\Resources\\img\\forgotPass.png');
+INSERT INTO `san pham` VALUES (9, 'Dragon Age 4 The Dread Wolf Rise', 'AAA', '4.1.1', 10.00, '2020-07-20', 'D:\\báo cáo EPU\\Đồ Án .Net\\Code\\Gamebook\\Server\\Resources\\img\\forgotPass.png');
+INSERT INTO `san pham` VALUES (10, 'Suicide Squad: Kill the Justice League', 'AAA', '2.3.5', 0.00, '2020-03-27', 'D:\\báo cáo EPU\\Đồ Án .Net\\Code\\Gamebook\\Server\\Resources\\img\\forgotPass.png');
+INSERT INTO `san pham` VALUES (11, 'Demo', 'Indie', '1.0.0', 10.00, '2020-12-06', 'D:\\báo cáo EPU\\Đồ Án .Net\\Code\\Gamebook\\Server\\Resources\\img\\forgotPass.png');
 
 -- ----------------------------
 -- Table structure for tinh trang don hang
@@ -190,6 +189,24 @@ CREATE TABLE `tinh trang don hang`  (
 INSERT INTO `tinh trang don hang` VALUES (1, 'Chưa xử lý');
 INSERT INTO `tinh trang don hang` VALUES (2, 'Đã chốt đơn');
 INSERT INTO `tinh trang don hang` VALUES (3, 'Hủy');
+
+-- ----------------------------
+-- View structure for spaaa
+-- ----------------------------
+DROP VIEW IF EXISTS `spaaa`;
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `spaaa` AS SELECT ID_sp,title,category,version,price ,DATE_FORMAT(`san pham`.`date release`,'%d/%m/%Y') as date,img_path from `san pham` WHERE category='AAA' ;
+
+-- ----------------------------
+-- View structure for spall
+-- ----------------------------
+DROP VIEW IF EXISTS `spall`;
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `spall` AS SELECT ID_sp,title,category,version,price ,DATE_FORMAT(`san pham`.`date release`,'%d/%m/%Y') as date,img_path from `san pham` ;
+
+-- ----------------------------
+-- View structure for spindie
+-- ----------------------------
+DROP VIEW IF EXISTS `spindie`;
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `spindie` AS SELECT ID_sp,title,category,version,price ,DATE_FORMAT(`san pham`.`date release`,'%d/%m/%Y') as date,img_path from `san pham`WHERE category='Indie' ;
 
 -- ----------------------------
 -- View structure for userkhach
