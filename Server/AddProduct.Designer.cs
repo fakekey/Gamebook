ec426b2fa8e95f66a5f081ea353279b94b9ae524
fakekey <camdalavip@gmail.com>
2020-12-19 16:54:50 +0700

    Them anh

--- Server/AddProduct.Designer.cs
+++ Server/AddProduct.Designer.cs
@@ -563,7 +563,7 @@ namespace Server
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(266, 43);
+            this.label1.Location = new System.Drawing.Point(311, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(223, 30);
             this.label1.TabIndex = 3;
